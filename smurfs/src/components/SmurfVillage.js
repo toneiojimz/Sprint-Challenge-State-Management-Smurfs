bb48c6d4bae9d2be0e581f138171af62cd192ae8
toneiojimz <ajimeneza77@gmail.com>
2019-12-14 09:31:49 -0800

    MPV and some style

--- smurfs/src/components/SmurfVillage.js
+++ smurfs/src/components/SmurfVillage.js
@@ -5,26 +5,36 @@ import {Smurf} from './Smurf';
 import styled from 'styled-components';
 
 
+
 const Villa = styled.div`
     display: flex;
     width: 900px;
     flex-wrap: wrap;
     margin-top: 20px;
-    
   
-    
-    
+`;
+
+const Img = styled.img`
+    backgroung-image: url(../images/smurf.png);
+`;
+
+const H1 =styled.h1`
+    font-size: 7rem;
+    color: skyblue;
 `;
 
 
 
-export const SmurfVillage =({GetSmurf, smurfs}) => {
+export const SmurfVillage =({GetSmurf, smurfs, isFetching}) => {
     useEffect(() =>{
         GetSmurf();
 
     }, [GetSmurf]);
+    if(isFetching){
+        // return <a> src= {smurf} alt="Smurf Image"</a>
+        return <H1> ....Getting Smurfed....</H1>
+    }
    
-    console.log(SmurfVillage)
     return(
         <Villa >
             {smurfs.map(sf =>(
